b8def30b8415c3cabb778b6d60dc963e7b3530a0
langens-jonathan <flowofcontrol@gmail.com>
2016-05-02 22:18:47 +0200

    changed name to title

--- addon/mixins/mu-authorization-access-token-mixin.js
+++ addon/mixins/mu-authorization-access-token-mixin.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default Ember.Mixin.create({
-    name: DS.attr('string'),
+    title: DS.attr('string'),
     description: DS.attr('string')
 });
--- addon/mixins/mu-authorization-authenticatable-mixin.js
+++ addon/mixins/mu-authorization-authenticatable-mixin.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default Ember.Mixin.create({
-    name: DS.attr('string')
+    title: DS.attr('string')
 });
